c67e0628c04690b9132bdbb07cba2f3bf4b8102c
JoeBew42 <joebew42@gmail.com>
2020-07-30 17:26:36 +0200

    Add more test cases for the route stopAt

--- src/test/java/RouteTest.java
+++ src/test/java/RouteTest.java
@@ -7,8 +7,10 @@ import static org.junit.Assert.assertEquals;
 public class RouteTest {
     @Test
     public void return_the_stop_at_a_specific_minute() {
-        Route route = new Route(of(1));
+        Route route = new Route(of(2, 4, 8));
 
-        assertEquals(valueOf(1), route.stopAtMinute(0));
+        assertEquals(valueOf(2), route.stopAtMinute(0));
+        assertEquals(valueOf(4), route.stopAtMinute(1));
+        assertEquals(valueOf(8), route.stopAtMinute(2));
     }
 }
