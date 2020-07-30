442da2591f920fe3b0b8549da76bab9dd5f21bc3
JoeBew42 <joebew42@gmail.com>
2020-07-30 19:14:25 +0200

    Remove duplicated constant

--- src/test/java/GossipingBusDriversTest.java
+++ src/test/java/GossipingBusDriversTest.java
@@ -1,11 +1,8 @@
 import org.junit.Test;
 
-import static java.lang.Integer.valueOf;
 import static org.junit.Assert.assertEquals;
 
 public class GossipingBusDriversTest {
-    public static final Integer NEVER = valueOf(-1);
-
     @Test
     public void when_not_all_gossips_are_exchanged_among_all_drivers_returns_never() {
         Driver driverA = new Driver(new Route(2, 1, 2));
@@ -31,6 +28,6 @@ public class GossipingBusDriversTest {
     }
 
     private void assertNever(Integer stopsNeeded) {
-        assertEquals(NEVER, stopsNeeded);
+        assertEquals(GossipingBusDrivers.NEVER, stopsNeeded);
     }
 }
