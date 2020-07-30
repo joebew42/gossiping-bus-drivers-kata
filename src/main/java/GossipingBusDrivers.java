70d59c65fec9d0d3ec76891a986c422c07ee3481
JoeBew42 <joebew42@gmail.com>
2020-07-30 19:23:16 +0200

    Store the drivers into a list

--- src/main/java/GossipingBusDrivers.java
+++ src/main/java/GossipingBusDrivers.java
@@ -1,16 +1,15 @@
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
 public class GossipingBusDrivers {
     public static final Integer MINUTES_IN_A_DAY = 480;
     public static final Integer NEVER = -1;
 
-    private Driver driverA;
-    private Driver driverB;
-
-    public GossipingBusDrivers(Driver driverA, Driver driverB) {
-        this.driverA = driverA;
-        this.driverB = driverB;
-    }
+    private List<Driver> drivers;
 
     public GossipingBusDrivers(Driver ... drivers) {
+        this.drivers = asList(drivers);
     }
 
     public Integer stopsNeededToShareAllGossips() {
@@ -25,11 +24,11 @@ public class GossipingBusDrivers {
     }
 
     private void exchangeAllGossipsAt(Integer minute) {
-        driverA.exchangeGossipsAtMinute(minute, driverB);
+        drivers.get(0).exchangeGossipsAtMinute(minute, drivers.get(1));
     }
 
     private boolean allGossipsExchanged() {
-        if (allGossipsExchangedFor(driverA) && allGossipsExchangedFor(driverB)) {
+        if (allGossipsExchangedFor(drivers.get(0)) && allGossipsExchangedFor(drivers.get(1))) {
             return true;
         }
         return false;
