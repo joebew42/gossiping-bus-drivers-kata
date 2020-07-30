bbefcbf653cc29717e46336be043201abb8610af
JoeBew42 <joebew42@gmail.com>
2020-07-30 18:01:57 +0200

    Remove unused imports

--- src/test/java/DriverTest.java
+++ src/test/java/DriverTest.java
@@ -1,8 +1,6 @@
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static java.lang.Integer.valueOf;
-import static java.util.List.of;
 import static org.junit.Assert.assertEquals;
 
 public class DriverTest {
