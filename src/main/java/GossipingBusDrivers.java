6bb84b53118ce3709227e0dac94a8f90faecd0ff
JoeBew42 <joebew42@gmail.com>
2020-07-30 18:56:37 +0200

    Introduce a first implementation

--- src/main/java/GossipingBusDrivers.java
+++ src/main/java/GossipingBusDrivers.java
@@ -1,8 +1,31 @@
 public class GossipingBusDrivers {
-    public GossipingBusDrivers(Driver ... drivers) {
+    public static final int MINUTES_IN_A_DAY = 480;
+
+    private Driver driverA;
+    private Driver driverB;
+
+    public GossipingBusDrivers(Driver driverA, Driver driverB) {
+        this.driverA = driverA;
+        this.driverB = driverB;
     }
 
     public Integer stopsNeededToShareAllGossips() {
+        for (int currentMinute = 0; currentMinute < MINUTES_IN_A_DAY; currentMinute++) {
+            if (allGossipsExchanged()) {
+                return currentMinute;
+            }
+        }
         return -1;
+    }
+
+    private boolean allGossipsExchanged() {
+        if (driverA.numberOfGossips() == numberOfDrivers() && driverB.numberOfGossips() == numberOfDrivers()) {
+            return true;
+        }
+        return false;
+    }
+
+    private int numberOfDrivers() {
+        return 2;
     }
 }
