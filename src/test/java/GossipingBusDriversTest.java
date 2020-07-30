63c97fb1c758700fffc43bf7b02342c1c64861cb
JoeBew42 <joebew42@gmail.com>
2020-07-30 19:16:11 +0200

    Rename tests

--- src/test/java/GossipingBusDriversTest.java
+++ src/test/java/GossipingBusDriversTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 public class GossipingBusDriversTest {
     @Test
-    public void when_not_all_gossips_are_exchanged_among_all_drivers_returns_never() {
+    public void when_not_all_gossips_are_exchanged_among_two_drivers_returns_never() {
         Driver driverA = new Driver(new Route(2, 1, 2));
         Driver driverB = new Driver(new Route(5, 2, 8));
 
@@ -14,7 +14,7 @@ public class GossipingBusDriversTest {
     }
 
     @Test
-    public void when_all_gossips_are_exchanged_among_all_drivers_returns_the_number_of_needed_stops() {
+    public void when_all_gossips_are_exchanged_among_two_drivers_returns_the_number_of_needed_stops() {
         Driver driverA = new Driver(new Route(2, 1, 2, 7));
         Driver driverB = new Driver(new Route(5, 2, 8, 7));
 
